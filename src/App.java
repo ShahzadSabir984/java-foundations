8a9294c8e714a5beabcfe7945b5de76a10a0767a
Shahzad Sabir <shahezadmaan@gmail.com>
2025-12-16 21:35:12 +0500

    Day 2: Java execution flow, JVM, and structured program

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App{
+    public static void main(String[] args) {
+
+        printName();
+        printGoal();
+    }
+        static void printName() {
+            System.out.println("My name is Shahzad.");
+        }
+
+        static void printGoal() {
+            System.out.println("I am building FAANG-LEVEL Java Foundations.");
+        }
+    }
